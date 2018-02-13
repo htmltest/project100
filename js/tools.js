d798f93129e9414261eabc99a1328cf4301b35b0
htmltest <struev.vs@gmail.com>
2018-02-13 09:18:55 +0300

    update main, style

--- js/tools.js
+++ js/tools.js
@@ -13,7 +13,29 @@ $(document).ready(function() {
         variableWidth: true,
         arrows: false,
         slidesToShow: 5,
-        centerMode: true
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 2000,
+        cssEase: 'linear'
+    });
+
+    $('.gallery').each(function() {
+        var curGallery = $(this);
+        curGallery.find('.gallery-hint-all').html(curGallery.find('.gallery-item').length);
+        curGallery.find('.gallery-hint-current').html('1');
+    });
+
+    $('.gallery-list').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>'
+    }).on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        var curGallery = $(this).parents().filter('.gallery');
+        curGallery.find('.gallery-hint-current').html(nextSlide + 1);
     });
 
 });
